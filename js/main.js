d164947ac1027a0889ace7478b4813026e853d2a
Moaz Ahmad <84990625+moaz1126@users.noreply.github.com>
2024-11-24 18:24:52 +0200

    Desktop Download Link Update

--- js/main.js
+++ js/main.js
@@ -11,3 +11,24 @@ document.querySelectorAll('a').forEach(link => {
     });
 });
   
+const DownloadLink = document.querySelector('#DownloadLink');
+
+// URL of the JSON file
+const url = 'https://cryptkeep-web.web.app/Desktop/releases.json';
+let lastReleaseUrl = 'https://github.com/moaz1126/CryptKeep_Desktop/releases/download/1.0.2/CryptKeep.Setup.1.0.2.exe';
+fetch(url)
+  .then(response => response.json())
+  .then(jsonData => {
+    // Access the releases array
+    const releases = jsonData.releases;
+
+    // Get the last release URL
+    lastReleaseUrl = releases[releases.length - 1].url;
+    UpdateAHref(lastReleaseUrl)
+  })
+  .catch(error => console.error('Error fetching the JSON file:', error));
+
+function UpdateAHref(url) {
+  console.log(url)
+  DownloadLink.setAttribute('href', url);
+}
